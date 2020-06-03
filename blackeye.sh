deb084b573d4dde2d5fef3e97d7e85927b6420e0
t-gitt <taheralkamel@gmail.com>
2020-06-03 09:39:50 +0800

    fix for #53 https://github.com/thelinuxchoice/blackeye/issues/53 "hostname: inavalid option --I"

--- blackeye.sh
+++ blackeye.sh
@@ -376,7 +376,7 @@ rm -rf sites/$server/usernames.txt
 
 fi
 
-default_ip=$(hostname -I)
+default_ip=$(hostname -i)
 printf "\e[1;92m[\e[0m*\e[1;92m] Put your local IP (Default %s): " $default_ip
 read ip
 ip="${ip:-${default_ip}}"
